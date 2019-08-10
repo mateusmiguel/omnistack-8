622d4b32bb780fded5db8e3e44be228f4d1701b9
Mateus Miguel <webmateusmiguel@gmail.com>
2019-08-10 11:19:32 -0300

    Init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+          <img src={logo} 
+          alt="Tindev" />
+  );
+}
+
+export default App;
